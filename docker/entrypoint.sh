7d32a18c5eeef788af4575c44a652a29087a69d9
miguelemosreverte <miguelemosreverte@gmail.com>
2022-05-07 21:12:51 +0000

    The onfiguration before was using a shared mount to access a file which contained the CLUSTER_ID This was supposed to work on minikube. It made sense in that context. But now we want to use it in a production kubernetes, meaning we are going to have multiple nodes, so sharing a file between the kafka instances is not possible, thus we need to set the CLUSTER_ID at the manifiest level, as an environment variable.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "STARTING ENTRYPOINT"
+
 NODE_ID=${HOSTNAME:6}
 LISTENERS="PLAINTEXT://:9092,CONTROLLER://:9093"
 ADVERTISED_LISTENERS="PLAINTEXT://kafka-$NODE_ID.$SERVICE.$NAMESPACE.svc.cluster.local:9092"
@@ -15,12 +17,8 @@ done
 
 mkdir -p $SHARE_DIR/$NODE_ID
 
-if [[ ! -f "$SHARE_DIR/cluster_id" && "$NODE_ID" = "0" ]]; then
-    CLUSTER_ID=$(kafka-storage.sh random-uuid)
-    echo $CLUSTER_ID > $SHARE_DIR/cluster_id
-else
-    CLUSTER_ID=$(cat $SHARE_DIR/cluster_id)
-fi
+
+echo "CLUSTER_ID, after it has been set, is $CLUSTER_ID"
 
 sed -e "s+^node.id=.*+node.id=$NODE_ID+" \
 -e "s+^controller.quorum.voters=.*+controller.quorum.voters=$CONTROLLER_QUORUM_VOTERS+" \
@@ -30,6 +28,8 @@ sed -e "s+^node.id=.*+node.id=$NODE_ID+" \
 /opt/kafka/config/kraft/server.properties > server.properties.updated \
 && mv server.properties.updated /opt/kafka/config/kraft/server.properties
 
-kafka-storage.sh format -t $CLUSTER_ID -c /opt/kafka/config/kraft/server.properties
+echo "CALLING USING kafka-storage.sh format --config /opt/kafka/config/kraft/server.properties --cluster-id $CLUSTER_ID"
+kafka-storage.sh format --config /opt/kafka/config/kraft/server.properties --cluster-id $CLUSTER_ID
+echo "CALLED SUCCESSFULLY"
 
 exec kafka-server-start.sh /opt/kafka/config/kraft/server.properties
